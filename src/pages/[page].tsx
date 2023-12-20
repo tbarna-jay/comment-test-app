1f3e7ab38a435c293416da1eb36fb75e7059cf20
Barna Tamas <barnatamas@gmail.com>
2023-12-20 18:38:32 +0100

    feat: count words count

--- src/pages/[page].tsx
+++ src/pages/[page].tsx
@@ -2,16 +2,20 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-type Comment = {
+interface Comment {
   postId: number;
   id: number;
   name: string;
   email: string;
   body: string;
-};
+}
+
+interface CommentWithMeta extends Comment {
+  wordNumber: number;
+}
 
 type CommentsPageProps = {
-  comments: Comment[];
+  comments: CommentWithMeta[];
   page: string;
 };
 
@@ -28,32 +32,36 @@ const Home: React.FC<CommentsPageProps> = ({ comments, page }) => {
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24`}
       >
-        <h1>Comments list page {page}</h1>
-        <div>
-          <button
-            disabled={page <= "1"}
-            onClick={() => router.push(`/${parseInt(page, 10) - 1}`)}
-          >
-            Previous
-          </button>
-          <button onClick={() => router.push(`/${parseInt(page, 10) + 1}`)}>
-            Next
-          </button>
+        <div className="p-4 bg-white rounded-lg shadow-md mb-4">
+          <h1>Comments list page {page}</h1>
+          <div>
+            <button
+              disabled={page <= "1"}
+              onClick={() => router.push(`/${parseInt(page, 10) - 1}`)}
+            >
+              Previous
+            </button>
+            <button onClick={() => router.push(`/${parseInt(page, 10) + 1}`)}>
+              Next
+            </button>
+          </div>
         </div>
         <ul>
-          {comments.map(({ id, name, email, body }) => (
-            <div key={id} className="p-4 bg-white rounded-lg shadow-md mb-4">
+          {comments.map(({ id, name, email, body, wordNumber }) => (
+            <li
+              key={id}
+              className="p-4 bg-white rounded-lg shadow-md mb-4 block"
+            >
               <div className="flex items-center gap-4">
                 <div>
-                  <h3 className="font-semibold text-sm">{name}</h3>
+                  <h2 className="font-semibold text-sm">
+                    {name} w: {wordNumber}
+                  </h2>
                   <time className="text-sm text-gray-500">{email}</time>
                 </div>
               </div>
               <p className="text-sm leading-loose text-gray-500 mt-4">{body}</p>
-              <span className="inline-block mt-2 px-2 py-1 bg-green-200 text-green-700 rounded-full">
-                Positive
-              </span>
-            </div>
+            </li>
           ))}
         </ul>
       </main>
@@ -65,15 +73,27 @@ export default Home;
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const page = context.params?.page as string;
+  let error = "";
+  let comments: Comment[] = [];
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/comments?_page=${page}&_limit=${commentsPerPage}`
   );
-  const comments = await res.json();
+  try {
+    comments = await res.json();
+  } catch (e) {
+    error = "Oops! We're having trouble loading the comments right now.";
+  }
+
+  const commentsWithWordNumber = comments.map((comment) => ({
+    ...comment,
+    wordNumber: comment.body.replace(/\n/g, " ").split(/\s+/).length,
+  }));
 
   return {
     props: {
-      comments,
+      comments: commentsWithWordNumber,
       page,
+      error,
     },
     revalidate: 600, // ISR - Revalidate every 10 minutes (600 seconds)
   };
